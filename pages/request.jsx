4940c0209dcde052030d45d6dfc4f4a8ac79b158
Cody <bittusmaths@gmail.com>
2023-06-12 22:24:51 +0530

    Done now

--- pages/request.jsx
+++ pages/request.jsx
@@ -12,9 +12,10 @@ export default function Request() {
   const [category, setCategory] = useState("Movies");
   const [year, setYear] = useState("");
   const [imdb, setImdb] = useState("");
+  const user = useUser();
 
   const addRequest = async () => {
-    if (name === "" && category === "") {
+    if (name === "" || category === "") {
       toast.error("Please fill all fields correctly");
       return;
     } else if (!user) {
@@ -33,9 +34,6 @@ export default function Request() {
       }
     }
   };
-
-  const supabaseClient = useSupabaseClient();
-  const user = useUser();
 
   return (
     <div className="isolate bg-black h-screen px-6 py-24 -z-10 sm:py-32 lg:px-8">
@@ -77,7 +75,7 @@ export default function Request() {
             >
               Name of the Movie or Series<span className="text-red-500">*</span>
             </label>
-            <div className="relative mt-2.5">
+            <div className="relative mt-2.5 text-black">
               <div className="absolute inset-y-0 left-0 flex items-center">
                 <label htmlFor="country" className="sr-only">
                   Category
@@ -87,7 +85,7 @@ export default function Request() {
                   name="country"
                   value={category}
                   onChange={(e) => setCategory(e.target.value)}
-                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-white focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                 >
                   <option>Movie</option>
                   <option>Serie</option>
@@ -98,7 +96,7 @@ export default function Request() {
                 onChange={(e) => setName(e.target.value)}
                 required
                 type="text"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-32 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-100 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                className="block w-full rounded-md border-0 px-3.5 py-2 pl-32 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-100 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
               />
             </div>
           </div>
@@ -115,7 +113,7 @@ export default function Request() {
                 value={year}
                 required
                 onChange={(e) => setYear(e.target.value)}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -131,7 +129,7 @@ export default function Request() {
                 type="text"
                 value={imdb}
                 onChange={(e) => setImdb(e.target.value)}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
